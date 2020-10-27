cf198889275d7424b62fe32f98772e943a10242e
Borshch <dorogina.e@gmail.com>
2020-10-28 00:00:00 +0300

    part 1.1

--- sandbox/src/main/java/MySecond.java
+++
@@ -1,5 +0,0 @@
-public class MySecond {
-    public static void main(String[] args) {
-        System.out.println("i had a problem");
-    }
-}
--- sandbox/src/main/java/ru/stqa/pft/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/MyFirstProgram.java
@@ -1,6 +1,7 @@
+package ru.stqa.pft;
+
 public class MyFirstProgram {
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        System.out.println("It's something bad");
     }
 }
