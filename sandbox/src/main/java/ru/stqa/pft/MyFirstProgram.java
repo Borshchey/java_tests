43b472a5f570fa40f7a70b8c2f552fab24aeb388
Borshch <dorogina.e@gmail.com>
2020-10-29 22:10:22 +0300

    1.5

--- sandbox/src/main/java/ru/stqa/pft/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/MyFirstProgram.java
@@ -2,6 +2,11 @@ package ru.stqa.pft;
 
 public class MyFirstProgram {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        String somebody = "world!";
+        System.out.println("Hello, " + somebody);
+
+        double l = 8.0;
+        double s = l * l;
+        System.out.println("Плошадь квадрата со стороной " + l + " равна " + s);
     }
 }
