70464289a44fb6fff0b7a4f7e9920a6572e1bf92
Borshch <dorogina.e@gmail.com>
2020-10-29 23:44:13 +0300

    1.9

--- sandbox/src/main/java/ru/stqa/pft/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/MyFirstProgram.java
@@ -6,23 +6,15 @@ public class MyFirstProgram {
     public static void main(String[] args) {
         hello("world");
         hello("user");
-        double l = 5;
 
-        System.out.println("Площадь квадрата со стороной " + l + " равна " + area(l));
+        Square s = new Square(5);
+        s.l = 5;
+        System.out.println("Площадь квадрата со стороной " + s.l + " равна " + s.area());
 
-        double a = 4;
-        double b = 6;
-        System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " равна " + area(a,b));
+        Rectangle r = new Rectangle(4,6);
+        System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " равна " + r.area());
     }
     public static void hello(String somebody) {
         System.out.println("Hello, " + somebody + "!");
-    }
-
-    public static double area(double len) {
-        return len * len;
-    }
-
-    public static double area (double a, double b) {
-        return a * b;
     }
 }
