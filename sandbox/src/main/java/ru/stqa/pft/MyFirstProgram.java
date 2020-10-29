45fea324e850ab19939dad3a95341f9c44def146
Borshch <dorogina.e@gmail.com>
2020-10-29 23:13:19 +0300

    1.6

--- sandbox/src/main/java/ru/stqa/pft/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/MyFirstProgram.java
@@ -1,12 +1,28 @@
 package ru.stqa.pft;
 
+import java.sql.SQLOutput;
+
 public class MyFirstProgram {
     public static void main(String[] args) {
-        String somebody = "world!";
-        System.out.println("Hello, " + somebody);
+        hello("world");
+        hello("user");
+        double l = 5;
 
-        double l = 8.0;
-        double s = l * l;
-        System.out.println("Плошадь квадрата со стороной " + l + " равна " + s);
+        System.out.println("Площадь квадрата со стороной " + l + " равна " + area(l));
+
+        double a = 4;
+        double b = 6;
+        System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " равна " + area(a,b));
+    }
+    public static void hello(String somebody) {
+        System.out.println("Hello, " + somebody + "!");
+    }
+
+    public static double area(double len) {
+        return len * len;
+    }
+
+    public static double area (double a, double b) {
+        return a * b;
     }
 }
