cff32bdf36e3c6843521349d4ded71b7ef2ba0c0
Borshchey <46170784+Borshchey@users.noreply.github.com>
2020-12-29 12:49:32 +0300

    ls 8.4

--- mantis-tests/src/main/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
+++ mantis-tests/src/main/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
@@ -14,35 +14,45 @@ import java.util.concurrent.TimeUnit;
 
 public class ApplicationManager {
   private final Properties properties;
-  WebDriver wd;
+  private WebDriver wd;
   private String browser;
+  private RegistrationHelper registrationHelper;
 
 
   public ApplicationManager(String browser) {
     this.browser = browser;
     properties = new Properties();
-
   }
 
 
   public void init() throws IOException {
     String target = System.getProperty("target", "local");
     properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
+  }
 
 
-    if (browser.equals(BrowserType.FIREFOX)) {
-      wd = new FirefoxDriver();
-    } else if (browser.equals(BrowserType.CHROME)) {
-      wd = new ChromeDriver();
-    } else if (browser.equals(BrowserType.IE)) {
-      wd = new InternetExplorerDriver();
-    }
-    wd.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
-    wd.get(properties.getProperty("web.baseUrl"));
+// Шаблон проектирования "Ленивая инициализация"
+
+    public WebDriver getDriver() {
+      if (wd == null) {
+        if (browser.equals(BrowserType.FIREFOX)) {
+          wd = new FirefoxDriver();
+        } else if (browser.equals(BrowserType.CHROME)) {
+          wd = new ChromeDriver();
+        } else if (browser.equals(BrowserType.IE)) {
+          wd = new InternetExplorerDriver();
+        }
+        wd.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+        wd.get(properties.getProperty("web.baseUrl"));
+
+      }
+      return wd;
   }
 
   public void stop() {
-    wd.quit();
+    if (wd != null) {
+      wd.quit();
+    }
   }
 
   public HttpSession newSession() {
@@ -51,5 +61,13 @@ public class ApplicationManager {
 
   public String getProperty(String key) {
     return properties.getProperty(key);
+  }
+
+  public RegistrationHelper registration() {
+    // return new RegistrationHelper(this);
+    if (registrationHelper == null) {
+      registrationHelper = new RegistrationHelper(this);
+    }
+    return registrationHelper;
   }
 }
